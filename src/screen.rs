f9431f59f65723765369f56f6411a6245a10f48a
gg-dev-05 <garvitgalgat@gmail.com>
2022-05-22 21:35:50 +0530

    bug

---
+++ src/screen.rs
@@ -0,0 +1,8 @@
+pub fn render(width: i32, height: i32) {
+  for _x in 0..height {
+    for _y in 0..width {
+      print!("0 ");
+    }
+    print!("\n");
+  }
+}
---
+++ src/state.rs
@@ -0,0 +1,4 @@
+pub fn state(width: u32, height: u32) {
+  let mut board = [[u32; width]; height];
+  println!("{:?}", board);
+}
