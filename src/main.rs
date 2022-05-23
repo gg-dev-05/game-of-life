93156d436364bf1f46ab7df5225b4d07d3909e00
gg-dev-05 <garvitgalgat@gmail.com>
2022-05-23 10:47:21 +0530

    bug fix + more patterns

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rand::Rng;
 use std::{thread, time::Duration};
 const WIDTH: usize = 50;
 const HEIGHT: usize = 10;
+const DELAY: u64 = 100;
 
 fn clear() {
     print!("\x1B[2J\x1B[1;1H");
@@ -17,7 +18,7 @@ fn draw(x: u8) {
     if x == 1 {
         print!("\x1b[92m{} \x1b[0m", "o");
     } else {
-        print!("\x1b[93m{} \x1b[0m", "O");
+        print!("\x1b[93m{} \x1b[0m", " ");
     }
 }
 
@@ -45,14 +46,19 @@ fn disp(array: [[u8; WIDTH]; HEIGHT], info: String) {
         }
         println!("");
     }
-    sleep(100);
+    sleep(DELAY);
 }
 
 fn simulate(front_screen: &mut [[u8; WIDTH]; HEIGHT]) {
     // let y_direction: [i8; 4] = [0, -1, 0, 1];
     // let x_direction: [i8; 4] = [-1, 0, 1, 0];
     // let temp: [[u8; WIDTH]; HEIGHT];
-    let mut temp = [[0u8; WIDTH]; HEIGHT];
+    let mut temp: [[u8; WIDTH]; HEIGHT] = [[0u8; WIDTH]; HEIGHT];
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            temp[y][x] = front_screen[y][x];
+        }
+    }
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
             let mut alive = 0;
@@ -68,21 +74,22 @@ fn simulate(front_screen: &mut [[u8; WIDTH]; HEIGHT]) {
             if x != WIDTH - 1 && front_screen[y][x + 1] == 1 {
                 alive += 1;
             }
-            // for i in 0..4 {
-            //     if 0 <= y as i8 + y_direction[i]
-            //         && y as i8 + y_direction[i] < HEIGHT as i8
-            //         && 0 <= x as i8 + x_direction[i]
-            //         && x as i8 + x_direction[i] < WIDTH as i8
-            //     {
-            //         if array[y + y_direction[i] as usize][x + x_direction[i] as usize] == 1 {
-            //             alive += 1;
-            //         }
-            //     }
-            // }
+            if y != 0 && x != 0 && front_screen[y - 1][x - 1] == 1 {
+                alive += 1;
+            }
+            if y != 0 && x != WIDTH - 1 && front_screen[y - 1][x + 1] == 1 {
+                alive += 1;
+            }
+            if y != HEIGHT - 1 && x != 0 && front_screen[y + 1][x - 1] == 1 {
+                alive += 1;
+            }
+            if y != HEIGHT - 1 && x != WIDTH - 1 && front_screen[y + 1][x + 1] == 1 {
+                alive += 1;
+            }
             if alive < 2 {
                 temp[y][x] = 0; // Any live cell with fewer than two live neighbours dies,
             }
-            if alive == 2 || alive == 3 {
+            if alive == 3 {
                 temp[y][x] = 1; // Any live cell with two or three live neighbours lives on to the next generation.
             }
             if alive > 3 {
@@ -90,7 +97,11 @@ fn simulate(front_screen: &mut [[u8; WIDTH]; HEIGHT]) {
             }
         }
     }
-    front_screen = temp;
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            front_screen[y][x] = temp[y][x];
+        }
+    }
 }
 
 fn main() {
@@ -99,8 +110,33 @@ fn main() {
     let mut generation = 1;
 
     randomize(&mut front_screen);
+    // front_screen[0 + 2][0 + 2] = 1;
+    // front_screen[0 + 2][1 + 2] = 1;
+    // front_screen[1 + 2][0 + 2] = 1;
+    // front_screen[1 + 2][1 + 2] = 1;
+
+    // front_screen[2 + 2][2 + 2] = 1;
+    // front_screen[2 + 2][3 + 2] = 1;
+    // front_screen[3 + 2][2 + 2] = 1;
+    // front_screen[3 + 2][3 + 2] = 1;
+
+    // front_screen[5][5] = 1;
+    // front_screen[6][5] = 1;
+    // front_screen[7][5] = 1;
+
+    // front_screen[3][3] = 1;
+    // front_screen[3][4] = 1;
+    // front_screen[3][5] = 1;
+    // front_screen[2][5] = 1;
+    // front_screen[1][4] = 1;
+
+    // front_screen[5][5] = 1;
+    // front_screen[6][5] = 1;
+    // front_screen[7][5] = 1;
+    // front_screen[6][4] = 1;
+    // front_screen[5][6] = 1;
     disp(front_screen, format!("Generation: 0"));
-    while generation < 100 {
+    while 1 == 1 {
         simulate(&mut front_screen);
         disp(front_screen, format!("Genration: {generation}"));
         generation += 1;
